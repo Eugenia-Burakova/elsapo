4c43bf9256fc9ad230966718980a0b553fedad72
EdelDenis <edeldenis12@gmail.com>
2022-02-07 23:19:45 +0200

    Block products improves

---
+++ javascript/block_products.js
@@ -0,0 +1,95 @@
+let more1 = document.querySelector('.more1');
+modal = document.querySelector('.modal1');
+close = document.querySelector('.close1');
+more1.addEventListener('click', function() {
+    modal.style.display = 'block';
+    modal.classList.add('modal-animation');
+});
+close.addEventListener('click', function() {
+    modal.style.display = 'none';
+    modal.classList.remove('modal-animation');
+});
+
+let more2 = document.querySelector('.more2');
+modal2 = document.querySelector('.modal2');
+close = document.querySelector('.close2');
+more2.addEventListener('click', function() {
+    modal2.style.display = 'block';
+    modal2.classList.add('modal-animation');
+});
+close.addEventListener('click', function() {
+    modal2.style.display = 'none';
+    modal2.classList.remove('modal-animation');
+});
+
+let more3 = document.querySelector('.more3');
+modal3 = document.querySelector('.modal3');
+close = document.querySelector('.close3');
+more3.addEventListener('click', function() {
+    modal3.style.display = 'block';
+    modal3.classList.add('modal-animation');
+});
+close.addEventListener('click', function() {
+    modal3.style.display = 'none';
+    modal3.classList.remove('modal-animation');
+});
+
+let more4 = document.querySelector('.more4');
+modal4 = document.querySelector('.modal4');
+close = document.querySelector('.close4');
+more4.addEventListener('click', function() {
+    modal4.style.display = 'block';
+    modal4.classList.add('modal-animation');
+});
+close.addEventListener('click', function() {
+    modal4.style.display = 'none';
+    modal4.classList.remove('modal-animation');
+});
+
+let more5 = document.querySelector('.more5');
+modal5 = document.querySelector('.modal5');
+close = document.querySelector('.close5');
+more5.addEventListener('click', function() {
+    modal5.style.display = 'block';
+    modal5.classList.add('modal-animation');
+});
+close.addEventListener('click', function() {
+    modal5.style.display = 'none';
+    modal5.classList.remove('modal-animation');
+});
+
+let more6 = document.querySelector('.more6');
+modal6 = document.querySelector('.modal6');
+close = document.querySelector('.close6');
+more6.addEventListener('click', function() {
+    modal6.style.display = 'block';
+    modal6.classList.add('modal-animation');
+});
+close.addEventListener('click', function() {
+    modal6.style.display = 'none';
+    modal6.classList.remove('modal-animation');
+});
+
+let more7 = document.querySelector('.more7');
+modal7 = document.querySelector('.modal7');
+close = document.querySelector('.close7');
+more7.addEventListener('click', function() {
+    modal7.style.display = 'block';
+    modal7.classList.add('modal-animation');
+});
+close.addEventListener('click', function() {
+    modal7.style.display = 'none';
+    modal7.classList.remove('modal-animation');
+});
+
+let more8 = document.querySelector('.more8');
+modal8 = document.querySelector('.modal8');
+close = document.querySelector('.close8');
+more8.addEventListener('click', function() {
+    modal8.style.display = 'block';
+    modal8.classList.add('modal-animation');
+});
+close.addEventListener('click', function() {
+    modal8.style.display = 'none';
+    modal8.classList.remove('modal-animation');
+});
