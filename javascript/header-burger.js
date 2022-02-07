ec8c3cfe4af5bf94c6df912f1a866fb20363c6c5
Vlad <peckovychv@gmail.com>
2022-02-07 14:57:20 +0200

    added burger-menu

---
+++ javascript/header-burger.js
@@ -0,0 +1,34 @@
+/*
+const iconMenu = document.querySelector('.burger_icon');
+if(iconMenu){
+    const menuBody = document.querySelector('.nav_menu');
+    iconMenu.addEventListener("click",function(e){
+        iconMenu.classList.toggle('._active');
+        menuBody.classList.toggle('._active');
+    });
+}
+*/
+
+(function () {
+    const iconBurger = document.querySelector(".burger_icon");
+    const navigationsBody = document.querySelector(".nav_menu");
+    const headerButtons = document.querySelector(".header__buttons");
+    iconBurger.addEventListener("click", function (e) {
+      document.body.classList.toggle("lock");
+      iconBurger.classList.toggle("active");
+      navigationsBody.classList.toggle("active");
+      headerButtons.classList.toggle("active");
+    });
+  
+    const navLinks = document.querySelectorAll(".header__buttons");
+    navLinks.forEach((navLink) => {
+      navLink.addEventListener("click", (_) => {
+            if (iconBurger.classList.contains("active")) {
+                document.body.classList.remove("lock");
+                iconBurger.classList.remove("active");
+                navigationsBody.classList.remove("active");
+            }
+          }
+      );
+    });
+  })();
