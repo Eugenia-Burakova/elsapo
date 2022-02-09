30b7e8b4b1a3cf3394d5be63ddcd77647f8580d9
Eugenia-Burakova <evzickabur@gmail.com>
2022-02-09 21:07:13 +0200

    about elsapo carousel added

--- javascript/our-location-map.js
+++ javascript/our-location-map.js
@@ -1,38 +1,36 @@
-// (function () {
-    function initMap() {
-        const elsapoCoordinates = { lat: 40.7596544, lng: -73.9785171 };
-        const map = new google.maps.Map(document.getElementById("map"), {
-            center: elsapoCoordinates,
-            zoom: 18,
-        });
+function initMap() {
+    const elsapoCoordinates = { lat: 40.7596544, lng: -73.9785171 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: elsapoCoordinates,
+        zoom: 18,
+    });
   
-        const contentString =
-            '<div id="content">' +
-            '<h1 class="firstHeading">ELSAPO</h1>' +
-            "<p>45 Rockefeller Plaza, New York, NY 10111</p>" +
-            "<p>USA</p>" +
-            "</div>";
+    const contentString =
+        '<div id="content">' +
+        '<h1 class="firstHeadingOfLocation">ELSAPO</h1>' +
+        "<p>45 Rockefeller Plaza, New York, NY 10111</p>" +
+        "<p>USA</p>" +
+        "</div>";
   
-        const infowindow = new google.maps.InfoWindow({
-            content: contentString,
-        });
+    const infowindow = new google.maps.InfoWindow({
+        content: contentString,
+    });
   
-        let marker = new google.maps.Marker({
-            position: elsapoCoordinates,
-            map,
-            title: "ELSAPO",
-        });
+    let marker = new google.maps.Marker({
+        position: elsapoCoordinates,
+        map,
+        title: "ELSAPO",
+    });
   
-        marker.addListener("click", () => {
-            infowindow.open({
-                anchor: marker,
-                map,
-            });
-        });
-  
-        marker = infowindow.open({
+    marker.addListener("click", () => {
+        infowindow.open({
             anchor: marker,
             map,
         });
-    }
-// })();
+    });
+  
+    marker = infowindow.open({
+        anchor: marker,
+        map,
+    });
+}
