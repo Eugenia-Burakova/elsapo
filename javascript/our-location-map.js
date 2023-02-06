79be59340c47dcc9d8587367ed7beecb7bbcc93f
Eugenia-Burakova <evzickabur@gmail.com>
2023-02-06 17:10:05 +0000

    map fixing

--- javascript/our-location-map.js
+++ javascript/our-location-map.js
@@ -1,54 +1,54 @@
 //first map
-// function initMap() {
-//     const elsapoCoordinates = { lat: 40.7596544, lng: -73.9785171 };
-//     const map = new google.maps.Map(document.getElementById("map"), {
-//         center: elsapoCoordinates,
-//         zoom: 18,
-//     });
+function initMap() {
+    const elsapoCoordinates = { lat: 40.7596544, lng: -73.9785171 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: elsapoCoordinates,
+        zoom: 18,
+    });
   
-//     const contentString =
-//         '<div id="content">' +
-//         '<h1 class="firstHeadingOfLocation">ELSAPO</h1>' +
-//         "<p>45 Rockefeller Plaza, New York, NY 10111</p>" +
-//         "<p>USA</p>" +
-//         "</div>";
+    const contentString =
+        '<div id="content">' +
+        '<h1 class="firstHeadingOfLocation">ELSAPO</h1>' +
+        "<p>45 Rockefeller Plaza, New York, NY 10111</p>" +
+        "<p>USA</p>" +
+        "</div>";
   
-//     const infowindow = new google.maps.InfoWindow({
-//         content: contentString,
-//     });
+    const infowindow = new google.maps.InfoWindow({
+        content: contentString,
+    });
   
-//     let marker = new google.maps.Marker({
-//         position: elsapoCoordinates,
-//         map,
-//         title: "ELSAPO",
-//     });
+    let marker = new google.maps.Marker({
+        position: elsapoCoordinates,
+        map,
+        title: "ELSAPO",
+    });
   
-//     marker.addListener("click", () => {
-//         infowindow.open({
-//             anchor: marker,
-//             map,
-//         });
-//     });
+    marker.addListener("click", () => {
+        infowindow.open({
+            anchor: marker,
+            map,
+        });
+    });
   
-//     marker = infowindow.open({
-//         anchor: marker,
-//         map,
-//     });
-// }
+    marker = infowindow.open({
+        anchor: marker,
+        map,
+    });
+}
 
 
 //second map
-function initMap() {
-    const elsapoCoordinates = { lat: 40.733744440225315, lng: -74.17103362510088 };
+// function initMap() {
+//     const elsapoCoordinates = { lat: 40.733744440225315, lng: -74.17103362510088 };
     
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 12,
-      center: elsapoCoordinates,
-    });
+//     const map = new google.maps.Map(document.getElementById("map"), {
+//       zoom: 12,
+//       center: elsapoCoordinates,
+//     });
     
-    const marker = new google.maps.Marker({
-      position: elsapoCoordinates,
-      map: map,
-    });
-}
-window.initMap = initMap;
+//     const marker = new google.maps.Marker({
+//       position: elsapoCoordinates,
+//       map: map,
+//     });
+// }
+// window.initMap = initMap;
